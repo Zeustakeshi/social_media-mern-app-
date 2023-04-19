f064e2cd989a67d33b3d3adff2038c5e62b211b4
Zeustakeshi <gaconmvp2312@gmail.com>
2023-04-20 01:34:45 +0700

    upload server url

--- client/src/utils/contst.ts
+++ client/src/utils/contst.ts
@@ -2,5 +2,5 @@ export const generateRandomImage = (q: string) => {
     return `https://source.unsplash.com/featured/?${q}`;
 };
 
-export const BASE_URL_API = "http://localhost:3000/api";
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL_API = "https://fakebook-4mvb.onrender.com/api";
+export const BASE_URL = "https://fakebook-4mvb.onrender.com";
