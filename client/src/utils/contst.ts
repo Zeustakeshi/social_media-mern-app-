db7bbeaf0370bd5ad724a7f0f8530fabc124270b
Zeustakeshi <gaconmvp2312@gmail.com>
2023-04-20 14:11:46 +0700

    upload

--- client/src/utils/contst.ts
+++ client/src/utils/contst.ts
@@ -2,9 +2,9 @@ export const generateRandomImage = (q: string) => {
     return `https://source.unsplash.com/featured/?${q}`;
 };
 
-// export const BASE_URL_API = "https://fakebook-4mvb.onrender.com/api";
-// export const BASE_URL = "https://fakebook-4mvb.onrender.com";
+export const BASE_URL_API = "https://fakebook-4mvb.onrender.com/api";
+export const BASE_URL = "https://fakebook-4mvb.onrender.com";
 
 // DEV
-export const BASE_URL_API = "http://localhost:3000/api";
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL_API = "http://localhost:3000/api";
+// export const BASE_URL = "http://localhost:3000";
