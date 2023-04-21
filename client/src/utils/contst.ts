30081a8599e1379525e0e36b74942d4d2dc9d62e
Zeustakeshi <gaconmvp2312@gmail.com>
2023-04-21 23:56:58 +0700

    upload

--- client/src/utils/contst.ts
+++ client/src/utils/contst.ts
@@ -1,6 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 export const generateRandomImage = (q: string) => {
     return `https://source.unsplash.com/featured/?${q}`;
 };
@@ -10,13 +7,15 @@ export const getCookie = (name: string) => {
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts?.pop()?.split(";").shift();
 };
+//"development" | "production"
+let NODE_ENV = "production";
 
 export const BASE_URL_API =
-    process.env.NODE_ENV === "development"
+    NODE_ENV === "development"
         ? "http://localhost:3000/api"
         : "https://fakebook-4mvb.onrender.com/api";
 
 export const BASE_URL =
-    process.env.NODE_ENV === "development"
+    NODE_ENV === "development"
         ? "http://localhost:3000"
         : "https://fakebook-4mvb.onrender.com";
